d7aa45fbc62ed0f8e2f1a6ca57c4e3dd33618933
benklenk <benklenk@outlook.com>
2018-08-18 21:19:07 -0700

    comment code

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Button from '@material-ui/core/Button'
 class App extends Component {
   constructor(props) {
     super(props);
+    // all the suffixes are hard coded and used to find rhymes in the findRHyme()
     this.state = {
       suffix: [
         'ito',
@@ -21,16 +22,19 @@ class App extends Component {
       loading: false,
       button: 'Generate a Nickname'
     }
+    // binding all function to this
     this.findRhyme = this.findRhyme.bind(this)
     this.noSpaces = this.noSpaces.bind(this)
     this.onClick = this.onClick.bind(this)
     this.pickRhyme = this.pickRhyme.bind(this)
   }
 
+  //3. pickRhyme chooses a random rhyme from this.state.rhymes
   pickRhyme() {
     return this.state.rhymes[Math.floor(Math.random() * this.state.rhymes.length)]
   }
 
+  //3.5. noSpaces checks if the rhyme picked by pickRhyme() is good (specifically if it is a near/perfect rhyme && if it has flags that RhymeBrain uses to determine if a return value is pronoucable && common). If it is not, noSpaces calls pickRhyme() again to find a new rhyming word. noSpaces then sets the rhyme state and sets the this.state.loading to FALSE
   noSpaces(suffix) {
     let rhyme = this.pickRhyme()
     while (rhyme.score < 263 || rhyme.flags !== 'bc') {
@@ -42,6 +46,7 @@ class App extends Component {
     this.setState({loading: false})
   }
 
+  // 1. onClick selects which suffix to use, calls findRhyme(), and sets state of the button and loading states
   onClick() {
     let suffix = this.state.suffix[Math.floor(Math.random() * this.state.suffix.length)]
     this.findRhyme(suffix)
@@ -49,6 +54,8 @@ class App extends Component {
     this.setState({button: 'Gimme Another'})
   }
 
+
+  // 2. findRhyme gets a JSON of all rhymes associated with the suffix passed in, and setsState({rhymes: JSON}) and calls noSpaces() to find a good rhyme. findRhyme() then edits displaySuffix so everything looks nice on the front end
   findRhyme(suffix) {
     fetch('http://rhymebrain.com/talk?function=getRhymes&word=' + suffix, {
       credentials: 'same-origin'
@@ -87,6 +94,7 @@ class App extends Component {
           justifyContent: 'center',
           alignItems: 'center'
         }} className="App"><br/>
+        {/* CSS is changed depending of if this.state.loading is TRUE, when it is the results are hidden and the button is disabled to prevent multiple requests from the user before the first loads */}
         <h1 className="Butter" style={this.state.loading
             ? {
               color: 'transparent',
